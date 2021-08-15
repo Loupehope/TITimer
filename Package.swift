cb0d9edc4324b9c185ba597e3fad10ef6a609009
Vlad Suhomlinov <>
2021-08-15 18:34:41 +0300

    refactor: обновлен Package

--- Package.swift
+++ Package.swift
@@ -9,23 +9,19 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TITimer",
             targets: ["TITimer"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TITimer",
             dependencies: []),
         .testTarget(
             name: "TITimerTests",
             dependencies: ["TITimer"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
